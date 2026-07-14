cb62546cfd8d6fd4de980442ac0b210d6f420789
Bildad Mbagara <bildad.dev@gmail.com>
2026-07-14 09:33:42 +0300

    Configure PostgreSQL connection and successfully start Spring Boot application

---
+++ src/main/java/com/bildadmbagara/campuscore/CampuscoreApplication.java
@@ -0,0 +1,13 @@
+package com.bildadmbagara.campuscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampuscoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampuscoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bildadmbagara/campuscore/CampuscoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bildadmbagara.campuscore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampuscoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
